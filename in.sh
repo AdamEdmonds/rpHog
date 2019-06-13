a90c398ccb38633c96bc543cb3eaa03cad76b420
Adam Edmonds <adam.edmonds@gmail.com>
2019-06-14 00:09:53 +0100

    setup install script

---
+++ in.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Install Node-Red
+bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+
+sudo systemctl enable nodered.service
